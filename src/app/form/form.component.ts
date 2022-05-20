d030fbe48d1d212958f174f97c1d62f1e8087a07
taquangnghia <nghiatqph13235@fpt.edu.vn>
2022-05-20 15:04:38 +0700

    first commit

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  inputValues={
+    id:0,
+    name:'',
+    age:0,
+    phone:0,
+    email:""
+
+  };
+
+users =[
+  {
+    id:1,
+    name:"nghia",
+    age:10,
+    email:"",
+    phone:100
+  }
+  
+]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // trung gian dữ liệu của file tsvs from
+ curee = null;
+  onSubmit(formData: {name: string, age: number,phone:number,email:string}) {
+    // console.log(formData);
+    // tìm id lớn nhất đang có để +1
+    const newUserIds = this.users 
+    .map(users=>users.id) // lấy ra mảng mứi chỉ có id 
+    .sort((a:number,b:number)=>b-a);
+    const maxId =  newUserIds[0];
+   if(this.curee==null ){
+    this.users.push({
+      ...formData,id:maxId+1
+    });
+   }else{
+    this.users.splice(this.curee,1,this.inputValues)
+   }
+   
+    // cập nhật gt của inputvalues về dèal
+    this.inputValues = {
+      id:0,
+      name:'',
+      age:0,
+      phone:0,
+      email:''
+      
+    }
+  }
+
+  
+  remove(id:number){
+    alert("ban muon xoa ko")
+    this.users = this.users.filter(item=>item.id !=id)
+  }
+  edit(index:any){
+    this.curee = index;
+    this.inputValues ={ ...this.users[index]};
+
+
+  }
+}
